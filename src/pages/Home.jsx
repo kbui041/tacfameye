a5ad1bb4e8687dec883b50f3424793ffe9d9b3e8
kbui041 <kbui04@uw.edu>
2025-06-18 17:11:22 -0700

    Add basic routing and page content

---
+++ src/pages/Home.jsx
@@ -0,0 +1,8 @@
+const Home = () => (
+  <section>
+    <h1>Welcome to Tacoma Family Eye Care</h1>
+    <p>Use the navigation above to explore our services and learn more about our clinic.</p>
+  </section>
+);
+
+export default Home;
